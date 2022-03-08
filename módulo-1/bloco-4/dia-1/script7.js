cbd41675ca6ecce75552631ef7d4042c61adad5d
polidex <lp.igor@gmail.com>
2022-03-08 16:11:55 -0300

    Prática - parte 1

---
+++ módulo-1/bloco-4/dia-1/script7.js
@@ -0,0 +1,17 @@
+const a = 33;
+const b = 11;
+
+let Adicao = a + b;
+  console.log(Adicao);
+
+let Subtracao = a - b;
+  console.log(Subtracao);
+
+let Multiplicacao = a * b;
+  console.log(Multiplicacao);
+
+let Divisao = a / b;
+  console.log(Divisao); 
+
+let Modulo = a % b;
+  console.log(Modulo);
