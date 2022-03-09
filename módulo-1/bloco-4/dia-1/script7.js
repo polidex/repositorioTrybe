da14ffda3bf37dff4c8d24f69141098569ac2480
polidex <lp.igor@gmail.com>
2022-03-09 15:42:37 -0300

    Exercício 8 pronto

--- módulo-1/bloco-4/dia-1/script7.js
+++ módulo-1/bloco-4/dia-1/script7.js
@@ -96,3 +96,23 @@ switch (pecaXadrez.toLowerCase()) {
 }
 
 console.log('-----Exercício 7-----');
+
+let nota = 32;
+
+if (nota < 0 || nota > 100) {
+  console.log('ERRO: A nota deve estar entre 0 e 100.'); 
+} else if (nota >= 90) {
+  console.log('A');
+} else if (nota >= 80) {
+  console.log('B');
+} else if (nota >= 70) {
+  console.log('C');
+} else if (nota >= 60) {
+  console.log('D');
+} else if (nota >= 50) {
+  console.log('E');
+} else if (nota < 50) {
+  console.log('F');
+}
+
+console.log('-----Exercício 8-----');
