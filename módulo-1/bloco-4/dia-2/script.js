b4fce6f977db428da7265b888ea8511d21072113
polidex <lp.igor@gmail.com>
2022-03-09 23:50:53 -0300

    Exercícios de Array feitos

---
+++ módulo-1/bloco-4/dia-2/script.js
@@ -0,0 +1,18 @@
+console.log('-----Exercício 1-----');
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = (menu[1]);
+
+console.log(menuServices);
+
+console.log('-----Exercício 2-----');
+
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+console.log('-----Exercício 3-----');
+
+  menu.push('Contato');
+
+console.log(menu);
