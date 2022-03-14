5679baaaac97a3437b0a426bf4b4e0de8b17939f
polidex <lp.igor@gmail.com>
2022-03-14 19:34:52 -0300

    exer. 2 feito

--- módulo-1/bloco-5/dia-1/script2.js
+++ módulo-1/bloco-5/dia-1/script2.js
@@ -1,1 +1,2 @@
 document.getElementsByTagName("p")[0].innerText = 'Daqui dois anos gostaria de estar num emprego legal e morando fora dos pais.';
+document.getElementsByClassName("main-content")[0].style.backgroundColor = "rgb(76,164,109)";
