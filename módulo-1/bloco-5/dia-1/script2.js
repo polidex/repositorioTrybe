6b30bc9c751f2549c629662138d49d29eacc8510
polidex <lp.igor@gmail.com>
2022-03-14 20:17:53 -0300

    Exer. 5 init

--- módulo-1/bloco-5/dia-1/script2.js
+++ módulo-1/bloco-5/dia-1/script2.js
@@ -6,3 +6,4 @@ document.getElementsByClassName("center-content")[0].style.backgroundColor = "wh
 
 document.getElementsByTagName("h1")[0].innerText = 'Exercício 5.1 - JavaScript';
 
+document.getElementsByTagName("p")[0].style.textTransform = "uppercase";
