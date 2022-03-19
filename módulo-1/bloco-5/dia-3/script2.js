21e07da9db1e3b16c8ac269698d606f2368450ac
polidex <lp.igor@gmail.com>
2022-03-18 23:47:55 -0300

    exer. 1 concluído

--- módulo-1/bloco-5/dia-3/script2.js
+++ módulo-1/bloco-5/dia-3/script2.js
@@ -17,16 +17,28 @@ createDaysOfTheWeek();
 function createDaysOfTheMounth() {
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
   const daysList = document.getElementById('days');
+  const holidays = [24, 25, 31];
+  const fridays = [4, 11, 18, 25];
 
   for (let i = 0; i < dezDaysList.length; i += 1) {
     const days = dezDaysList[i];
     const createDays = document.createElement('li');
     createDays.innerHTML = days;
     createDays.classList.add("day");
-    
-    
-    daysList.appendChild(createDays);
+
+    for (let iHD = 0; iHD < holidays.length; iHD += 1) {
+      if (dezDaysList[i] === holidays[iHD]) {
+        createDays.classList.add("holiday");
+      }
+    }
+
+    for (let iFD = 0; iFD < fridays.length; iFD += 1) {
+      if (dezDaysList[i] === fridays[iFD]) {
+        createDays.classList.add("friday");
+      }
+    }
+      daysList.appendChild(createDays);
+    }
   }
-}
   
 createDaysOfTheMounth();
