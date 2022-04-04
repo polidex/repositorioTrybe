8fa90d8b13cf9fd45d5e3ebe6e4b0183caf07499
polidex <lp.igor@gmail.com>
2022-04-04 20:13:44 -0300

    parte II.1 pronto

---
+++ módulo-1/bloco-7/dia-1/parte-II.js
@@ -0,0 +1,15 @@
+//-------------------Desafio 1----------
+
+const factorial = number => {
+  let result = 1;
+
+  for (let index = 2; index <= number; index += 1) {
+      result *= index;
+  }
+
+  return result;
+}
+
+console.log(factorial(5));
+
+//-------------------Desafio 2----------
