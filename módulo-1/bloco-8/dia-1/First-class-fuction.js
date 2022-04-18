ecde745b9e1717ab4a4b2c1546ac7494dac8a507
Igor Lungato Polido <93943042+polidex@users.noreply.github.com>
2022-04-17 21:58:33 -0300

    files fix

---
+++ módulo-1/bloco-8/dia-1/First-class-fuction.js
@@ -0,0 +1,38 @@
+// First-class Function - basicamente são funções q são atriuidas a variáveis.
+
+// Exemplos:
+
+function sum (number1, number2) {
+  return number1 + number2;
+}
+
+const sumVariable = sum;
+
+console.log(sumVariable);
+
+// A função sum foi atribuida a variavel sumVariable, para mostrar a capacidade do JavaScript de aramazenar uma função dentro de uma variavel.
+
+const sum = (number1, number2) => {
+  return number1 + number2;
+};
+
+// Arrow function mostra justamente a capacidade do JS de armazenar funções em variaveis.
+
+const sayHello = () => {
+  return ('hello trybers');
+}
+
+const printGreeting = (callback) => {
+    console.log(callback());
+}
+
+printGreeting(sayHello); 
+
+// outro exemplo
+
+const sumFixAmount = (amount) => {
+  return (number) => amount + number;
+}
+
+const initialSum = sumFixAmount(15)
+console.log(initialSum(5)); 
