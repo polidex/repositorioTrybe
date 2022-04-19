7c477b3eba6c2592e874a774a9dbe00fa7c983df
polidex <lp.igor@gmail.com>
2022-04-19 18:53:56 -0300

    bloco 9 iniciado

---
+++ módulo-1/bloco-9/README.md
@@ -0,0 +1,6 @@
+Bloco 09: JavaScript e Testes Assíncronos
+
+Dia 01: JavaScript Assíncrono e Callbacks
+Dia 02: JavaScript Assíncrono - Fetch API e async/await
+Dia 03: Jest - Testes Assíncronos
+Dia 04: Projeto - Carrinho de Compras
---
+++ módulo-1/bloco-9/dia-1/operações-assíncronas.js
@@ -0,0 +1,3 @@
+/* Operações assíncronas - consiste em fazer com que funções sejão executadas de forma assincrona, ou seja, 
+se houver uma fução que leva algum tempo pra excutar o código continua executando indepemdente dessa função 
+ter terminado de executar.*/
