927aa83d11e3c873e5c3ff00ea2ed06ae2476757
polidex <lp.igor@gmail.com>
2022-04-25 20:32:18 -0300

    anotações dia 9.3

---
+++ módulo-1/bloco-9/dia-3/resumo.js
@@ -0,0 +1,7 @@
+/*Checar extenção chrome -JSON Formatter-
+
+Para funções nativamente promeças é possível usar async/await ou then;
+Para funções comuns que precisam ser asincronas só é possivel usar async/await;
+ */
+
+const { name, id, height, wieght } 
